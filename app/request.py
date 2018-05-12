49dc8b025e0094742a3d1bf43cdc9156bd745170
Richard <richi@localhost.localdomain>
2018-05-12 15:18:53 +0300

    Adding a function that gets json data

--- app/request.py
+++ app/request.py
@@ -1,4 +1,32 @@
 import urllib.request, json
 from .models import Sources
 
-# def configure_request(app):
+api_key = None
+sources_url = None
+
+def configure_request(app):
+    global api_key, sources_url
+    api_key = app.config['NEWS_API_KEY']
+    sources_url = app.config['SOURCES_BASE_URL']
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = sources_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_results(sources_results_list)
+
+def process_results(sources_list):
+    '''
+    Function that processes the json results
+    '''
+    pass
--- config.py
+++ config.py
@@ -3,11 +3,11 @@ class Config:
     '''
     General configuration for the parent class
     '''
-    SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={general}&apiKey={}'
-    EVERYTHING_LOCATION_BASE_URL = 'https://newsapi.org/v2/everything?sources={reuters}&apiKey={}'
-    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={30}&apiKey={}'
-    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={30}&apiKey={}'
-    
+    SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_LOCATION_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={}&apiKey={}'
+    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={}&apiKey={}'
+
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -30,6 +30,6 @@ class DevConfig:
     DEBUG = True
 
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
+    'development':DevConfig,
+    'production':ProdConfig
 }
