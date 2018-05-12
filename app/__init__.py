bc606412da6b8070dea86c8d604353f4ccbe8814
Richard <richi@localhost.localdomain>
2018-05-12 13:37:31 +0300

    Initializing the app

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    # Registering the BluePrint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting the configurations
+    from .request import conf
