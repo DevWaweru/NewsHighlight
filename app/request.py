a5efd070d29aeb5d99d85586d8c92b89714c4c7a
Richard <richi@localhost.localdomain>
2018-05-12 19:20:20 +0300

    Adding function that gets articles based on id

--- app/request.py
+++ app/request.py
@@ -9,6 +9,7 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     # api_key = app.config['NEWS_API_KEY']
     sources_url = app.config['SOURCES_BASE_URL']
+    articles_per_source_url = app.config['EVERYTHING_LOCATION_BASE_URL']
 
 
 def get_sources(category):
@@ -24,8 +25,7 @@ def get_sources(category):
         sources_results = None
 
         if get_sources_response['sources']:
-            sources_results_list = get_sources_response['sources']
-            sources_results = process_results(sources_results_list)
+            sources_results = process_results(get_sources_response['sources'])
     
     return sources_results
 
@@ -48,3 +48,20 @@ def process_results(sources_list):
             sources_results.append(source_object)
     
     return sources_results
+
+def get_articles(id):
+    '''
+    Function that gets articles based on the source id
+    '''
+    get_article_location_url = articles_per_source_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_article_location_url) as url:
+        articles_location_data = url.read()
+        articles_location_response = json.loads(articles_location_data)
+
+        articles_location_results = None
+
+        if articles_location_response['articles']:
+            articles_location_results = process_articles(articles_location_response['articles'])
+        
+    return articles_location_results
