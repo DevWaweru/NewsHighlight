fe3bbf6aa92ec8a3b83d7f94f10c9b4ade19fa3d
Richard <richi@localhost.localdomain>
2018-05-12 13:20:07 +0300

    Setting up the server and adding api variable

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     EVERYTHING_LOCATION_BASE_URL = 'https://newsapi.org/v2/everything?sources={reuters}&apiKey={}'
     TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={30}&apiKey={}'
     EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize={30}&apiKey={}'
+    
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig:
---
+++ manage.py
@@ -0,0 +1,13 @@
+from app import create_app
+from flask_script import Manager, Server
+
+# create app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+# @manager.command
+
+if __name__=='__main__':
+    manager.run()
