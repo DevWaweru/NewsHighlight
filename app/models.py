2201588a07efb5aef518e9b0c19f9e09d497069f
Richard <richi@localhost.localdomain>
2018-05-12 14:31:30 +0300

    Initializing Sources class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,15 @@
-# class Sources:
-#     '''
-#     Sources class that defines source objects
-#     '''
-#     def __ini
+class Sources:
+    '''
+    Sources class that defines source objects
+    '''
+    def __init__(self,id,name,description,url,category,country):
+        '''
+        Function that initiates the sources class
+        '''
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        
--- app/request.py
+++ app/request.py
@@ -1,1 +1,4 @@
-import urllib.request,json
+import urllib.request, json
+from .models import Sources
+
+# def configure_request(app):
