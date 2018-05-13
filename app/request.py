a29ef80a1f815a79b8acf8aeb4447bd9971a1f25
Richard <richi@localhost.localdomain>
2018-05-13 14:39:10 +0300

    Adding function that gets topheadlines

--- app/request.py
+++ app/request.py
@@ -5,12 +5,16 @@ from datetime import datetime
 api_key = None
 sources_url = None
 articles_url = None
+topheadlines_url = None
+everything_url = None
 
 def configure_request(app):
-    global api_key, sources_url, articles_url
+    global api_key, sources_url, articles_url, topheadlines_url, everything_url
     api_key = app.config['NEWS_API_KEY']
     sources_url = app.config['SOURCES_BASE_URL']
     articles_url = app.config['EVERYTHING_SOURCE_BASE_URL']
+    topheadlines_url = app.config['TOP_HEADLINES_BASE_URL']
+    everything_url = app.config['EVERYTHING_BASE_URL']
 
 
 def get_sources(category):
@@ -88,3 +92,20 @@ def process_articles(my_articles):
             article_location_list.append(article_source_object)
         
     return article_location_list
+
+def topheadlines(limit):
+    '''
+    Function that gets articles based on the source id
+    '''
+    get_topheadlines_url = topheadlines_url.format(limit,api_key)
+
+    with urllib.request.urlopen(get_topheadlines_url) as url:
+        topheadlines_data = url.read()
+        topheadlines_response = json.loads(topheadlines_data)
+
+        topheadlines_results = None
+
+        if topheadlines_response['articles']:
+            topheadlines_results = process_articles(topheadlines_response['articles'])
+        
+    return topheadlines_results
